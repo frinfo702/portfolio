8ca900f25a7aab61ab27513c7c4e529c503e4123
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2026-06-19 12:31:39 +0900

    delete background animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import FluidField from "./components/FluidField";
 
 export default function Home() {
   return (
     <div className="relative flex flex-col flex-1 items-center bg-black font-sans text-[#e5e5e5]">
-      <FluidField />
       <main className="relative z-10 flex flex-col w-full max-w-xl px-6 py-20 sm:px-8">
         <header className="flex items-center justify-between mb-4">
           <h1 className="text-base font-semibold text-white">
